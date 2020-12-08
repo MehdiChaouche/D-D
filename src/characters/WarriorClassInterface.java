f88385f73ccd27fa332b6abc189626ddc42c84b8
Mehdi <mehdi.chaouche@le-campus-numerique.fr>
2020-12-08 09:28:14 +0100

    Initializing Warrior & Wizard classes + their interfaces

---
+++ src/characters/WarriorClassInterface.java
@@ -0,0 +1,7 @@
+package characters;
+
+public interface WarriorClassInterface {
+
+	String toString();
+
+}
---
+++ src/characters/WizardClassInterface.java
@@ -0,0 +1,7 @@
+package characters;
+
+public interface WizardClassInterface {
+
+	String toString();
+
+}
